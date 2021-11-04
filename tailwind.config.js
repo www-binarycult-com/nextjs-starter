eb939aab6b038174c77e163b2dc5d7de68ac0ff4
dennispio <dennis.piotrowski@web.de>
2021-11-04 18:51:11 +0100

    feat: add new artikel page

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,12 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    screens: {
+      sm: '575px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+    },
     extend: {},
   },
   variants: {
