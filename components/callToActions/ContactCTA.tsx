3674a72f4b9f3a17f5f820e25bdcc74823fad72a
Dennis Piotrowski <dennis.piotrowski@web.de>
2022-12-10 21:02:05 +0100

    refactor: add smooth scroll to anchor

--- components/callToActions/ContactCTA.tsx
+++ components/callToActions/ContactCTA.tsx
@@ -1,8 +1,8 @@
 export const ContactCTA = () => {
   return (
-    <div className='h-[80vh] flex justify-center items-center bg-yellow-400'>
+    <div className='h-[80vh] flex justify-center items-center bg-black'>
       <a href='#contact-form'>
-        <h4 className='text-3xl font-extrabold'>Kostenlose Erstberatung</h4>
+        <h4 className='text-3xl font-extrabold hover-underline-animation text-black'>Kostenlose Erstberatung</h4>
       </a>
     </div>
   );
