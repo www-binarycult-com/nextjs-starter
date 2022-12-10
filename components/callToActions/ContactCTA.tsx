a3571e769e711a0433d5de9163a72cb2fa0e988c
Karim Ould Mahieddine <karimouldmahieddine@MacBook-Pro-von-Karim.local>
2022-12-10 19:55:19 +0100

    added contact form

---
+++ components/callToActions/ContactCTA.tsx
@@ -0,0 +1,9 @@
+export const ContactCTA = () => {
+  return (
+    <div className='h-[80vh] flex justify-center items-center bg-yellow-400'>
+      <a href='#contact-form'>
+        <h4 className='text-3xl font-extrabold'>Kostenlose Erstberatung</h4>
+      </a>
+    </div>
+  );
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,13 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
 import { Slider } from '../components/slider';
+import { ProjectProvider } from '../components/project/ProjectProvider';
+import { ProjectList } from '../components/project/ProjectList';
+import { Services } from '../components/services/Services';
+import { Hero } from '../components/hero/Hero';
+import { TextImageSection } from '../components/text-image/TextImageSection';
+import { ContactForm } from '../components/form/ContactForm';
+import { ContactCTA } from '../components/callToActions/ContactCTA';
 
 const images = [
   { href: '/', src: '/1.png' },
@@ -10,12 +17,6 @@ const images = [
   { href: '/', src: '/5.png' },
   { href: '/', src: '/6.png' },
 ];
-import { ProjectProvider } from '../components/project/ProjectProvider';
-import { ProjectList } from '../components/project/ProjectList';
-import { Headline } from '../components/Headline';
-import { Services } from '../components/services/Services';
-import { Hero } from '../components/hero/Hero';
-import { TextImageSection } from '../components/text-image/TextImageSection';
 
 const Home: NextPage = () => {
   return (
@@ -32,36 +33,33 @@ const Home: NextPage = () => {
           text='Lorem ipsum dolor sit amet, officia excepteur ex fugiat reprehenderit enim labore culpa sint ad nisi Lorem pariatur mollit ex esse exercitation amet. Nisi anim cupidatat excepteur officia. Reprehenderit nostrud nostrud ipsum Lorem est aliquip amet voluptate voluptate dolor minim nulla est proident. Nostrud officia pariatur ut officia. Sit irure elit esse ea nulla sunt ex occaecat reprehenderit commodo officia dolor Lorem duis laboris cupidatat officia voluptate. Culpa proident adipisicing id nulla nisi laboris ex in Lorem sunt duis officia eiusmod. Aliqua reprehenderit commodo ex non excepteur duis sunt velit enim. Voluptate laboris sint cupidatat ullamco ut ea consectetur et est culpa et culpa duis.'
           image={{ src: './coder.svg' }}
         />
+        <ContactCTA />
+        <ProjectProvider>
+          <ProjectList />
+        </ProjectProvider>
         <TextImageSection
           heading='Ueberschrift'
           text='Lorem ipsum dolor sit amet, officia excepteur ex fugiat reprehenderit enim labore culpa sint ad nisi Lorem pariatur mollit ex esse exercitation amet. Nisi anim cupidatat excepteur officia. Reprehenderit nostrud nostrud ipsum Lorem est aliquip amet voluptate voluptate dolor minim nulla est proident. Nostrud officia pariatur ut officia. Sit irure elit esse ea nulla sunt ex occaecat reprehenderit commodo officia dolor Lorem duis laboris cupidatat officia voluptate. Culpa proident adipisicing id nulla nisi laboris ex in Lorem sunt duis officia eiusmod. Aliqua reprehenderit commodo ex non excepteur duis sunt velit enim. Voluptate laboris sint cupidatat ullamco ut ea consectetur et est culpa et culpa duis.'
         />
+        <Services />
+        <Slider images={images} />
         <TextImageSection
           heading='Ueberschrift'
           reversedOrder
           text='Lorem ipsum dolor sit amet, officia excepteur ex fugiat reprehenderit enim labore culpa sint ad nisi Lorem pariatur mollit ex esse exercitation amet. Nisi anim cupidatat excepteur officia. Reprehenderit nostrud nostrud ipsum Lorem est aliquip amet voluptate voluptate dolor minim nulla est proident. Nostrud officia pariatur ut officia. Sit irure elit esse ea nulla sunt ex occaecat reprehenderit commodo officia dolor Lorem duis laboris cupidatat officia voluptate. Culpa proident adipisicing id nulla nisi laboris ex in Lorem sunt duis officia eiusmod. Aliqua reprehenderit commodo ex non excepteur duis sunt velit enim. Voluptate laboris sint cupidatat ullamco ut ea consectetur et est culpa et culpa duis.'
           image={{ src: './coder.svg' }}
         />
-        <ProjectProvider>
-          <ProjectList />
-        </ProjectProvider>
-        <Services />
-        <Slider images={images} />
+        <div id='contact-form' className='flex flex-col-reverse md:flex-row'>
+          <div className='w-full md:w-1/2'>
+            <ContactForm />
+          </div>
+          <div className='w-full p-10 md:w-1/2'>
+            Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat. Lorem
+            ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat. Lorem ipsum
+            dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat. Lorem ipsum dolor
+          </div>
+        </div>
       </main>
-      <footer>
-        <p className='h-screen '>Binarycult website</p>
-        <Headline tag='h1'>Überschrift</Headline>
-        <br />
-        <Headline tag='h2'>Überschrift</Headline>
-        <br />
-        <Headline tag='h3'>Überschrift</Headline>
-        <br />
-        <Headline tag='h4'>Überschrift</Headline>
-        <br />
-        <Headline tag='h5'>Überschrift</Headline>
-        <br />
-        <p className='h-screen bg-green-100'>Binarycult website</p>
-      </footer>
     </div>
   );
 };
