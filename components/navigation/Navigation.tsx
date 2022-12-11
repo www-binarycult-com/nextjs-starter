88dcebb05a392d4343b1d762e6c502774d6743ff
Karim Ould Mahieddine <karimouldmahieddine@MacBook-Pro-von-Karim.local>
2022-12-11 18:04:22 +0100

    added nav

--- components/navigation/Navigation.tsx
+++ components/navigation/Navigation.tsx
@@ -1,9 +1,32 @@
+import classNames from 'classnames';
+import { useState } from 'react';
+
 export const Navigation = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const burgerClassNames = classNames('fixed space-y-2 z-50', isOpen ? 'menu-open' : 'menu-closed');
+  const menuClassNames = classNames(
+    'fixed bg-yellow-400 w-full h-screen top-0 left-0 z-40 flex justify-center items-center',
+    isOpen ? 'opacity-100' : 'opacity-0 hidden'
+  );
   return (
-    <button className='space-y-2'>
-      <div className='w-8 h-0.5 bg-gray-600'></div>
-      <div className='w-8 h-0.5 bg-gray-600'></div>
-      <div className='w-8 h-0.5 bg-gray-600'></div>
-    </button>
+    <menu>
+      <button onClick={() => setIsOpen(!isOpen)} className={burgerClassNames}>
+        <div className='w-8 h-1 bg-gray-600'></div>
+        <div className='w-8 h-1 bg-gray-600'></div>
+        <div className='w-8 h-1 bg-gray-600'></div>
+      </button>
+      <div className={menuClassNames}>
+        <div>
+          <ul>
+            <li>
+              <a href='/'>Home</a>
+            </li>
+            <li>
+              <a href='/#contact-form'>Kontankt</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </menu>
   );
 };
