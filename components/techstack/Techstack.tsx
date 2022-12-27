fd15d6fefb25c797f35b426251f6a52518932594
Dennis Piotrowski <dennis.piotrowski@web.de>
2022-12-27 14:42:10 +0100

    refactor: change texte

--- components/techstack/Techstack.tsx
+++ components/techstack/Techstack.tsx
@@ -15,7 +15,6 @@ const frontendStack: TechStackTypes = [
   { src: 'frontend/tailwindcss.svg', title: 'Tailwind CSS' },
   { src: 'frontend/typescript.svg', title: 'Typescript' },
   { src: 'frontend/javascript.svg', title: 'Javascript' },
-  { src: 'frontend/wordpress.svg', title: 'Wordpress' },
   { src: 'frontend/gatsby.svg', title: 'Gatsby' },
   { src: 'frontend/nextjs.svg', title: 'NextJS' },
   { src: 'more.svg', title: 'More' },
@@ -40,6 +39,14 @@ const devOpsStack: TechStackTypes = [
   { src: 'devOps/azure.svg', title: 'Microsoft Azure' },
   { src: 'devOps/docker.svg', title: 'Docker' },
   { src: 'devOps/kubernetes.svg', title: 'Kubernetes' },
+  { src: 'more.svg', title: 'More' },
+];
+
+const cmsStack: TechStackTypes = [
+  { src: 'cms/wordpress.svg', title: 'Wordpress' },
+  { src: 'cms/sanity.svg', title: 'Sanity' },
+  { src: 'cms/shopify.svg', title: 'Shopify' },
+  { src: 'cms/netlify.svg', title: 'Netlify' },
   { src: 'more.svg', title: 'More' },
 ];
 
@@ -127,7 +134,7 @@ export const TechStack = () => {
               }}
               ref={ref4}
             >
-              Dev Ops
+              CMS
             </TechHeadline>
           </div>
           <div
@@ -139,39 +146,25 @@ export const TechStack = () => {
           </div>
           <div className='bg-yellow-200 pt-8'>
             <TechItem stack={frontendStack} href='' hoveredPosition={position?.position} position={1} title='Frontend'>
-              Wir bei immajung sind stolz darauf, ein umfassendes Angebot an digitalen Dienstleistungen anzubieten, um
-              unsere Kunden bei ihrem Streben nach Erfolg zu unterstützen. Von der Webentwicklung und dem Hosting bis
-              hin zu technischer SEO und UI/UX-Design verfügt unser Expertenteam über die Fähigkeiten und die Erfahrung,
-              um Ihr Unternehmen in der digitalen Welt zum Erfolg zu führen. Mit dem Schwerpunkt auf moderner Technik
-              und benutzerfreundlichem Design sind wir bestens gerüstet, um Ihrem Unternehmen zu Wachstum und Erfolg zu
-              verhelfen. Kontaktieren Sie uns noch heute, um mehr über unser Full-Service-Angebot zu erfahren und wie
-              Ihr Unternehmen davon profitieren kann.
+              Im Frontend setzen wir auf leistungsstarke und zukunftsorientierte Technologien. Unser Tech-Stack umfasst
+              React, Next, Gatsby, SolidJS und CSS, die wir in Kombination mit dem CSS-Framework Tailwind einsetzen.
+              Zusätzlich verwenden wir three.js, eine JavaScript-Bibliothek, die es uns ermöglicht, interaktive
+              3D-Grafiken und Animationssequenzen in unsere Webseiten zu integrieren.
             </TechItem>
             <TechItem stack={backendStack} href='' hoveredPosition={position?.position} position={2} title='Backend'>
-              Als Digitalagentur in Hannover bietet immajung fachkundige UI/UX-Design-Services, um unseren Kunden zu
-              helfen, benutzerfreundliche, ansprechende und visuell ansprechende Websites zu erstellen. Mit dem Fokus
-              auf moderne Designprinzipien und nutzerzentriertes Design kann unser Expertenteam Ihnen helfen, eine
-              Website zu erstellen, die nicht nur gut aussieht, sondern auch ein nahtloses und angenehmes Nutzererlebnis
-              bietet. Setzen Sie sich noch heute mit uns in Verbindung, um mehr darüber zu erfahren, wie unsere
-              UI/UX-Designservices Ihrem Unternehmen zugutekommen und Ihnen zum Erfolg in der digitalen Welt verhelfen
-              können.{' '}
+              Im Backend setzen wir auf leistungsstarke und zukunftssichere Technologien. Unser Tech-Stack umfasst
+              NodeJS, Typescript, GoLang und verschiedene Datenbanken wie PostgreSQL und MongoDB.
             </TechItem>
             <TechItem stack={mobileStack} href='' hoveredPosition={position?.position} position={3} title='Mobile'>
-              Als führende Digitalagentur in Hannover, Deutschland, bietet immajung zuverlässige und sichere
-              Hosting-Services zur Unterstützung Ihrer Online-Aktivitäten. Mit dem Fokus auf Betriebszeit, Leistung und
-              Sicherheit sind unsere Hosting-Lösungen so konzipiert, dass Ihre Website reibungslos und effizient läuft,
-              so dass Sie sich auf das Wachstum Ihres Unternehmens konzentrieren können. Kontaktieren Sie uns noch
-              heute, um mehr über unsere Hosting-Services zu erfahren und wie Ihr Unternehmen davon profitieren kann.
+              Im Bereich Mobile setzen wir auf Flutter und React-native, zwei leistungsstarke Frameworks für die
+              Entwicklung von nativen Apps für Android und iOS.
             </TechItem>
-            <TechItem stack={devOpsStack} href='' hoveredPosition={position?.position} position={4} title='Dev Ops'>
-              Wir bei immajung wissen, wie wichtig die Suchmaschinenoptimierung (SEO) für moderne Unternehmen ist.
-              Deshalb bieten wir umfassende technische SEO-Dienstleistungen an, die unseren Kunden helfen, ihre
-              Suchmaschinenplatzierungen zu verbessern und mehr Besucher auf ihre Websites zu bringen. Von der
-              Keyword-Recherche und On-Page-Optimierung bis hin zum Linkaufbau und der Erstellung von Inhalten - unsere
-              technischen SEO-Experten verfügen über das Wissen und die Erfahrung, um Ihrem Unternehmen zum Erfolg in
-              der digitalen Welt zu verhelfen. Setzen Sie sich noch heute mit uns in Verbindung, um mehr darüber zu
-              erfahren, wie wir Ihr Unternehmen dabei unterstützen können, sein Suchmaschinenranking zu verbessern und
-              mehr Besucher auf Ihre Website zu bringen.
+            <TechItem stack={cmsStack} href='' hoveredPosition={position?.position} position={4} title='CMS'>
+              Unsere Agentur bietet auch Unterstützung bei der Wahl und Einführung von Content Management Systemen
+              (CMS). Wir haben Erfahrung mit verschiedenen CMS wie Wordpress, Shopify, Sanity, Contentfull und
+              netlify-cms. Wir beraten Sie gerne bei der Wahl des passenden CMS für Ihre Anforderungen und unterstützen
+              Sie bei der Einführung und Nutzung des Systems. Auf Wunsch übernehmen wir auch die Pflege und Wartung
+              Ihres CMS.
             </TechItem>
           </div>
         </div>
